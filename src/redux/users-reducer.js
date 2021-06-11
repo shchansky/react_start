7c61a383b823a86d4aafad859dec7c27837e8452
shchansky <gavrilenko71285@gmail.com>
2021-06-11 16:35:34 +0300

    55/56 pagination

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,14 +1,18 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET__CURRENT_PAGE = 'SET__CURRENT_PAGE';
+const SET__TOTAL_USERS_COUNT = 'SET__TOTAL_USERS_COUNT';
 
 
 
 let initialState = {
 
 
-    users: [   ],
-
+    users: [],
+    pageSize: 10,
+    totalUsersCount: 0,
+    currentPage: 4,
 
 
 
@@ -20,38 +24,49 @@ const usersReducer = (state = initialState, action) => {
 
 
     switch (action.type) {
-        case FOLLOW: 
+        case FOLLOW:
 
-        return {
-            ...state, 
-            // users: [...state.users]
-            // users: state.users.map (u => u)
-            users: state.users.map ( u => {
-                if (u.id === action.userId) {
-                    return {...u, followed: true}
-                }
-                return u;
-            })
-        }
-        case UNFOLLOW: 
-        return {
-            ...state, 
-            users: state.users.map ( u => {
-                if (u.id === action.userId) {
-                    return {...u, followed: false}
-                }
-                return u;
-            })
-        }
-        case SET_USERS: 
-        return {...state, users: [...state.users, ...action.users]}
-        default: return state;
+            return {
+                ...state,
+                // users: [...state.users]
+                // users: state.users.map (u => u)
+                users: state.users.map(u => {
+                    if (u.id === action.userId) {
+                        return { ...u, followed: true }
+                    }
+                    return u;
+                })
+            }
+        case UNFOLLOW:
+            return {
+                ...state,
+                users: state.users.map(u => {
+                    if (u.id === action.userId) {
+                        return { ...u, followed: false }
+                    }
+                    return u;
+                })
+            }
+        case SET_USERS:
+            return { ...state, users: action.users }
+
+
+        case SET__CURRENT_PAGE:
+            return { ...state, currentPage: action.currentPage }
+
+        case SET__TOTAL_USERS_COUNT:
+            return { ...state, totalUsersCount: action.count}
+
+        default:
+            return state;
     }
 }
 
 export const followAC = (userId) => ({ type: FOLLOW, userId })
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
 export const setUsersAC = (users) => ({ type: SET_USERS, users })
+export const setCurrentPageAC = (currentPage) => ({ type: SET__CURRENT_PAGE, currentPage })
+export const setTotalUsersCountAC = (totalUsersCount) => ({ type: SET__TOTAL_USERS_COUNT, count:totalUsersCount})
 
 
 export default usersReducer
