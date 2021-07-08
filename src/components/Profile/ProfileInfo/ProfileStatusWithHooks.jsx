be690371a4175015ab2abf519774ac48581946ce
shchansky <gavrilenko71285@gmail.com>
2021-07-09 00:29:43 +0300

    hook, useEffect - refactoring code

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -12,11 +12,11 @@ const ProfileStatusWithHooks = (props) => {
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
 
-    useEffect(() => { setStatus (props.status)})
+    useEffect(() => { setStatus (props.status)}, [props.status])
 
 
 
-    
+  
     const activateEditMode = () => {
         setEditMode(true);
     }
